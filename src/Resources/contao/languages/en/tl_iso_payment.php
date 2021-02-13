1abbcd57b50f702817663b2d56fe43d3ce1e58a3
Carsten Kollmeier <carsten@ckollmeier.de>
2021-02-13 23:36:31 +0100

    Fixes

--- src/Resources/contao/languages/en/tl_iso_payment.php
+++ src/Resources/contao/languages/en/tl_iso_payment.php
@@ -3,19 +3,17 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the Wangaz\ContaoIsotopeWirecardBundle.
+ * This file is part of the Kollmeier\ContaoIsotopePaypalSDKBundle.
  *
- * (c) Wangaz
- * (c) inspiredminds
+ * (c) Carsten Kollmeier
  *
  * @license CC-BY-SA-4.0
  */
 
+
 $GLOBALS['TL_LANG']['tl_iso_payment'] = array_merge($GLOBALS['TL_LANG']['tl_iso_payment'], [
-  $GLOBALS['TL_LANG']['tl_iso_payment'] = array_merge($GLOBALS['TL_LANG']['tl_iso_payment'], [
-      'paypalSDKClientId' => ['Client ID', 'Your Paypal API Client ID.'],
-      'paypalSDKSecret' => ['Secret', 'Your Paypal API Secret.'],
-      'paypalSDKSBClientId' => ['Sandbox Client ID', 'Your Paypal API Client ID.'],
-      'paypalSDKSBSecret' => ['Sandbox Secret', 'Your Paypal API Secret.'],
-  ]);
+    'paypalSDKClientId' => ['Client ID', 'Your Paypal API Client ID.'],
+    'paypalSDKSecret' => ['Secret', 'Your Paypal API Secret.'],
+    'paypalSDKSBClientId' => ['Sandbox Client ID', 'Your Paypal API Client ID.'],
+    'paypalSDKSBSecret' => ['Sandbox Secret', 'Your Paypal API Secret.'],
 ]);
